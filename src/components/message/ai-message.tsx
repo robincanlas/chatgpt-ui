4f2c5dd22c54de58ab9c3289dbb57e46f65be0b0
Kristoffer Robin Canlas <kristofferrobincanlas@gmail.com>
2024-09-21 23:52:51 +0800

    added blinking cursor

--- src/components/message/ai-message.tsx
+++ src/components/message/ai-message.tsx
@@ -1,5 +1,6 @@
 import Typewriter from 'typewriter-effect';
 import useMessagesStore, { Message, STREAMING } from '../../store/messages';
+import "./message.css";
 
 interface AiMessageProps {
   message: Message;
@@ -23,6 +24,7 @@ const AiMessage = ({
         options={{
           cursorClassName: 'typewriter-cursor',
           delay: 10,
+          cursor: '',
         }}
       />}
     </>
